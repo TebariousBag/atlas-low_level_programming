0a9c3652e22ba00d020553a4bc4fcbab005b2ecf
“Tristian <tristian.davis@atlasschool.com>
2024-01-21 20:52:32 +0000

    testing

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -7,6 +7,7 @@
 int main(void)
 {
 	int n;
+	int ld;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
@@ -14,11 +15,11 @@ int main(void)
 	ld = n % 10
 
 	if (ld > 5);
-	printf("Last digit of %i is %i is greater than 5\n");
+		printf("Last digit of %i is %i is greater than 5\n");
 	else if (ld == 0);
-	printf("Last digit of %i is %i and is 0\n");
+		printf("Last digit of %i is %i and is 0\n");
 	else if(ld < 6);
-	printf("Last digit of %i is %i and is less than 6\n");
+		printf("Last digit of %i is %i and is less than 6\n");
 	
 	return (0);
 }
