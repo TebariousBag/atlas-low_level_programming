e28eb85ba14985deba16d3bec04a5986f4c943ac
“Tristian <tristian.davis@atlasschool.com>
2024-01-21 01:36:16 +0000

    byte

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 /**
  *
@@ -5,12 +6,17 @@
 
 int main(void)
 {
+	char c;
+	int i;
+	long l;
+	long long ll;
+	float f;
 
-	printf("Size of a char: %i\n", sizeof(char));
-	printf("Size of an int: %i\n", sizeof(int));
-	printf("Size of a long int: %i\n", sizeof(long int));
-	printf("Size of a long long int: %i\n", sizeof(long long int));
-	printf("Size of a float: %i\n", sizeof(float));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(l));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(ll));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 
 	return (0);
 }
