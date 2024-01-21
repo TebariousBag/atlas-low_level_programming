7eebc1ba2dff8f75f0de8ba92887dd5d3b4823bd
“Tristian <tristian.davis@atlasschool.com>
2024-01-21 20:55:37 +0000

    ;

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -14,11 +14,11 @@ int main(void)
 	
 	ld = n % 10
 
-	if (ld > 5);
+	if (ld > 5)
 		printf("Last digit of %i is %i is greater than 5\n");
-	else if (ld == 0);
+	else if (ld == 0)
 		printf("Last digit of %i is %i and is 0\n");
-	else if(ld < 6);
+	else if(ld < 6)
 		printf("Last digit of %i is %i and is less than 6\n");
 	
 	return (0);
