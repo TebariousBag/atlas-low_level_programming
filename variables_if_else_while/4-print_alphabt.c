fd6d40ff69a758358c9243845429c779dabb7d3c
“Tristian <tristian.davis@atlasschool.com>
2024-01-21 21:55:23 +0000

    4 test

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/*
+ * 
+*/
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c<'z')
+	{
+		if (c != 'q' && c != 'e')
+		{
+			putchar(c);
+		}
+		c++;
+	}
+
+	putchar('\n');
+	return (0);	
+}
