ad5c959fd3d3d8cf2ea3b58b65190b8e49aee901
“Tristian <tristian.davis@atlasschool.com>
2024-01-21 20:25:25 +0000

    push to save

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	
+	ld = n % 10
+
+	if (ld > 5);
+	printf("Last digit is greater than 5"
+	else if (ld == 0);
+	printf("and is 0");
+	else if(ld < 6);
+	printf("and is less than 6");
+	/* your code goes there */
+	return (0);
+}
