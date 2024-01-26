2fa41cfc61cbb3e7813ab7360302db74ccdeff69
“Tristian <tristian.davis@atlasschool.com>
2024-01-26 23:36:22 +0000

    3test

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *_islower - checks for lower case letter
+Return: either a 1 or a 0
+*@c: parameter that we are checking
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
