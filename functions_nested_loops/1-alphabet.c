4e85db32af16fc8895e2bda6c4c01fbfa8b903d4
“Tristian <tristian.davis@atlasschool.com>
2024-01-26 22:14:13 +0000

    1 test

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - function prints a-z
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char i;
+	for (i = 'a' ; i <= 'z' ; i++)
+		_putchar(i);
+	_putchar('\n');
+}
