180edc3d0f36c10042ae565b45abc7b34a9a3403
“Tristian <tristian.davis@atlasschool.com>
2024-01-27 00:55:21 +0000

    test6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_abs - function prints absoulte zero
+ *@i: parameter
+Return: -a or a
+ */
+int _abs(int)
+{
+	if (i < 0)
+		return (-i);
+	else
+	{
+		return (i);
+	}
+}
