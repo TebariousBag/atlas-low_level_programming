3c4483816ca8142e201216cd383b9665a98c6f70
“Tristian <tristian.davis@atlasschool.com>
2024-01-28 00:03:56 +0000

    0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,13 @@
 #include "main.h"
 /**
- *
+ * _isupper - function check if lower case
+ * @c: parameter
+ * Return: 1 or 0
  */
 
 int _isupper(int c)
 {
-	if (c >= 'A' && c <+ 'Z')
+	if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
 		return (0);
