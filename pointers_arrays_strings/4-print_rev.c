dbf1ad0332ec5703a8595602a509e884db991888
“Tristian <tristian.davis@atlasschool.com>
2024-01-28 23:04:11 +0000

    test4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_rev - function prints the string in reverse
+ * @s: tje text that is gonna print
+ * Return: void
+ */
+
+void print_rev(char *s)
+
+{
+	int index;
+	int counter;
+
+	for (index = 0 ; s[index] != '\0' ; index++)
+		counter++;
+	for (index = counter -1 ; index >= 0 ; index--)
+		_putchar(s[index]);
+	_putchar('\n');
+}
