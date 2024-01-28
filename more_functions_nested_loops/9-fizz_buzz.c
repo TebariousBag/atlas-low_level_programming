574efbb09ca262bec201a5a1f331717ca515540b
“Tristian <tristian.davis@atlasschool.com>
2024-01-28 03:55:48 +0000

    Buzz 100

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
- *
+ * main - function prints numbers 1-100 except multiples of 3 and 5, then its replaced with fizz or buzz
+ * Return: void
  */
 
 int main(void)
@@ -16,11 +17,13 @@ int main(void)
 			printf("Fizz ");
 
 		else if ((i % 5) == 0)
-			printf("Buzz ");
+		{
+			if (i != 100)
+				printf("Buzz ");
 
-		else if (i == 100)
-			printf("Buzz");
-
+			else
+				printf("Buzz");
+		}
 		else
 			printf("%d ", i);
 	}
