85866dcf2c7ac76008f7632d58bf443df365399e
“Tristian <tristian.davis@atlasschool.com>
2024-01-28 00:25:30 +0000

    2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul(int a, int b) - function multiplies a and b
+ * @a: parameter 1
+ * @b: parameter 2
+ * Return: a * b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
