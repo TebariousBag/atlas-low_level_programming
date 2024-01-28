14cf5649054ebc5ba429285f1a7308611095e8b0
“Tristian <tristian.davis@atlasschool.com>
2024-01-28 03:02:51 +0000

    8test

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_square - function prints squares
+ * @size: parameter size of square
+ * Return: void
+ */
+
+void print_square(int size)
+
+{
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		int row, column;
+
+		for (row = 1 ; row <= size ; row++)
+		{
+			for (column = 1 ; column <= size ; column++)
+				_putchar('#');
+			_putchar('\n');
+		}
+			
+	}
+}
