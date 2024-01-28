1e0ccb26c380027ada532cd3f131aa8bce900403
“Tristian <tristian.davis@atlasschool.com>
2024-01-28 00:13:58 +0000

    1

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,11 +1,13 @@
 #include "main.h"
 /**
- *
+ * _isdigit - function check if number 0-9
+ * @c: parameter
+ * Return: 0 or 1
  */
 
 int _isdigit(int c)
 {
-	if (c <= 0 && >= 9)
+	if (c <= '0' && c >= '9')
 		return (1);
 	else
 		return (0);
