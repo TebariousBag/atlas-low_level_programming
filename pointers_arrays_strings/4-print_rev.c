66b7d1d67079d1f7d037c29029f3fc46f428157c
“Tristian <tristian.davis@atlasschool.com>
2024-01-28 23:16:22 +0000

    betty

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -13,7 +13,7 @@ void print_rev(char *s)
 
 	for (index = 0 ; s[index] != '\0' ; index++)
 		counter++;
-	for (index = counter -1 ; index >= 0 ; index--)
+	for (index = counter - 1 ; index >= 0 ; index--)
 		_putchar(s[index]);
 	_putchar('\n');
 }
