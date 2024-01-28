8db95bbbeca63a37cec2641f83c7f7e46e97e8d5
“Tristian <tristian.davis@atlasschool.com>
2024-01-28 21:40:21 +0000

    0test

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,12 @@
 #include "main.h"
 /**
- *
+ * reset_to_98 - function changes the value of what n is pointing to
+ * @n: parameter to be changed
+ * Return: void
  */
 
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 
 {
-
+	*n = 98;
 }
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
 void reset_to_98(int *n);
-_putchar;
+int _putchar(char c);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
