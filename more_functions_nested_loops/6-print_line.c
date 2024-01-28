3437d6ae00451f55c49f2e2c8a6945663aca7ca7
“Tristian <tristian.davis@atlasschool.com>
2024-01-28 01:37:56 +0000

    6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_line - draws lines
+ * @n: parameter with the number that will print
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		int i;
+
+		for (i = 1 ; i <= n ; i++)
+			_putchar('_');
+		_putchar('\n');
+	}
+}
