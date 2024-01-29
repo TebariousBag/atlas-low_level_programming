ef8fbb72c050cddde0add2b3cbdd68d4016f1e3d
“Tristian <tristian.davis@atlasschool.com>
2024-01-29 21:39:21 +0000

    test

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -3,23 +3,28 @@
  *
  */
 
-int _atoi(char *s) {
-    int sign = 1;
-    int result = 0;
+int _atoi(char *s)
+{
 
-    while (*s == ' ') s++;
+	int sign = 1;
+	int result = 0;
 
-    if (*s == '-') {
-        sign = -1;
-        s++;
-    } else if (*s == '+') {
-        s++;
-    }
+	while (*s == ' ') s++;
 
-    while (*s >= '0' && *s <= '9') {
-        result = result * 10 + (*s - '0');
-        s++;
-    }
+    	if (*s == '-')
+	{
+	sign *= -1;
+	s++;
+	}
+	else if (*s == '+')
+	{
+		s++;
+	}
+	while (*s >= '0' && *s <= '9')
+	{
+		result = result * 10 + (*s - '0');
+		s++;
+	}
 
-    return (result * sign);
+	return (result * sign);
 }
