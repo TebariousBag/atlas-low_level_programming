939088c11a7db7d2ea2a5876657576f8441c2658
“Tristian <tristian.davis@atlasschool.com>
2024-01-29 19:46:16 +0000

    test

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -9,11 +9,17 @@
 char *_strcpy(char *dest, char *src)
 
 {
-        int index;
+	char *i = dest;
 
-        for (index = 0 ; src[index] != '\0' ; index++)
-                dest[index] = src[index];
-        dest[index] = '\0';
-        return (dest);
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+
+	*dest = '\0';
+
+	return i;
 }
 
