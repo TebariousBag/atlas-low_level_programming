7d167d8a26475bfe348b0717db639fd56eb3a946
“Tristian <tristian.davis@atlasschool.com>
2024-02-08 23:24:23 +0000

    1test

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strncat - function joins 2 strings, not all bytes
+ * @dest: parameter 1
+ * @src: parameter 2
+ * @n: parameter 3
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+
+{
+	int count = 0;
+	int counter = 0;
+
+	while (dest[count] != '\0')
+	{
+		count++;
+	}
+	while (src[counter] < n && src[counter] != '\0')
+	{
+		dest[count] = src[counter];
+		count++;
+		counter++;
+	}
+	dest[count] = '\0';
+	return (dest);
+}
