dd3eaa30602891b53dc4b4948c83e2b26cdd3bd9
“Tristian <tristian.davis@atlasschool.com>
2024-02-08 22:51:00 +0000

    0test

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,28 @@
 #include "main.h"
 /**
- *
+* _strcat - function concatenates two strings, which means joining them into one
+* @dest: parameter 1
+* @src: parameter 2
+* Return: dest
 */
 
-char *strcat(char *dest, char *src);
+char *_strcat(char *dest, char *src)
+
+{
+	int count = 0;
+	int counter = 0;
+
+	while (dest[count] != '\0')
+	{
+		count++;
+	}
+	while (src[counter] != '\0')
+	{
+		dest[count] = src[counter];
+		count++;
+		counter++;
+	}
+
+	dest[count] = '\0';
+	return (dest);
+}
