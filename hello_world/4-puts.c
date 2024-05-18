8c10a7e18bec6b6ccdde0f057b21a9884a475a3c
“Tristian <tristian.davis@atlasschool.com>
2024-05-18 23:17:23 +0000

    test

---
+++ hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - prints text that was asked
+* Return: 0
+**/
+
+	int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+		return (0);
+}
