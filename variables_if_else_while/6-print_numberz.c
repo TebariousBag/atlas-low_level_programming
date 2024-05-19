7cd66e03ef1b27b5daee19ef378a8f278dc33a34
“Tristian <tristian.davis@atlasschool.com>
2024-05-19 20:08:32 +0000

    confused

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
+
 /**
- * main - Numberz
+ * main - numberz
  * Description: print single digit numbers of base 10 withou using variable char
  * Return: 0
  */
 
 int main(void)
 {
-	int c = '0';
+	int c = 0;
 
-	while (c < 10)
+	while (c <= 9)
 	{
 		putchar(48 + c);
 		c++;
