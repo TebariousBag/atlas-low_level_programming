03e19720be130f96cbc5ebe58ebdeb13353a937e
“Tristian <tristian.davis@atlasschool.com>
2024-05-19 00:45:28 +0000

    forgota

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -14,7 +14,7 @@
 
 	printf("Size of a char: %zu byte(s)\n", sizeof(c));
 	printf("Size of an int: %zu byte(s)\n", sizeof(i));
-	printf("Size of long int: %zu byte(s)\n", sizeof(l));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(l));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(ll));
 	printf("Size of a float: %zu byte(s)\n", sizeof(f));
 
