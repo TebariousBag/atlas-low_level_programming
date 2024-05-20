b3150322337feccf0cd26be70e3d49f9c8636885
“Tristian <tristian.davis@atlasschool.com>
2024-05-20 22:16:52 +0000

    saving8start

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day
+ * Description: prints every minute of the day
+ * Return: 0
+ */
+
+void jack_bauer(void)
+{
+	int hour, minute;
+
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (minute = 0; minute < 60; minute++)
+		{
+
+}
