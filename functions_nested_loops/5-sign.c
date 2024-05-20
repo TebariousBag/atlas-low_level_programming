136b798647a43858de48c6bb210f4e66595af529
“Tristian <tristian.davis@atlasschool.com>
2024-05-20 21:02:54 +0000

    5test

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_sign - checks if positive negative
+ * @n: the parameter
+ * Return: 1 if positive, 0 if 0, or -1 if negative
+ */
+
+int print_sign(int n)
+{
+	if (n > '0')
+		_putchar('1');
+		return (1);
+	else if (n == 0)
+		_putchar('0');
+		return (0);
+	else (n < 0)
+		_putchar('-');
+		return (-1);
+}
