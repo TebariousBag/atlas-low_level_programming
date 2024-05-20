160cbd7a3691b1e20d0bd04efcbfb5efdfb40fbd
“Tristian <tristian.davis@atlasschool.com>
2024-05-20 23:33:03 +0000

    test

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * times_table - times table of 9
+ * Return: void
+ */
+
+void times_table(void)
+{
+	int row, column, result;
+
+	for (row = 0; row < 10; row++)
+	{
+		for (column = 0; column < 10; column++)
+		{
+			result = row * column;
+			if (column == 0)
+			{
+				_putchar(result + '0');
+			}
+			if (column < 10 && column != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(result + '0');
+			}
+			else if (column >= 10)
+			{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((column / 10) + '0');
+					_putchar((column % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
