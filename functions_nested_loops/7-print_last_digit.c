62e5d63e2dfb81f1f46677ddc203f676d4a975c1
“Tristian <tristian.davis@atlasschool.com>
2024-05-20 21:41:49 +0000

    7testing

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit
+ * @p: parameter
+ * Return: returns the value of last digit
+ */
+
+int print_last_digit(int p)
+{
+	int last;
+
+	last = p % 10;
+
+	if (p < 0)
+	{
+		return (-last);
+	}
+	else
+		return (last);
+
+}
