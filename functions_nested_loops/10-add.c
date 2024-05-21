8ac36b7c240d28c201a030772258b39b45267f4d
“Tristian <tristian.davis@atlasschool.com>
2024-05-21 00:06:12 +0000

    10add

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @t: parameter
+ * @d: parameter
+ * Return: returns the addition of two integers
+ */
+
+int add(int t, int d)
+{
+	return (t + d);
+}
