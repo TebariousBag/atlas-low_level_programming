1cd10065d6550e4449481a91582cda02c7a2dbc4
“Tristian <tristian.davis@atlasschool.com>
2024-05-22 18:35:28 +0000

    3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0-9
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+		_putchar(" %i", num);
+		num++;
+	return (0);
+}
