5216a2eece1179652dab320bc83a4f3538e75d1c
“Tristian <tristian.davis@atlasschool.com>
2024-05-22 19:11:32 +0000

    test

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 0-14 ten times
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int num;
+	int lines;
+
+	for (lines = 1; lines < 10; lines++)
+	{
+		for (num = 0; num <= 14; num++)
+	}
+	_putchar('\n');
+}
