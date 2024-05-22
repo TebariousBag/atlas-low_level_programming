26c6ec9f56ed34a8fbdbe5f6dec2cdc51f765eb7
“Tristian <tristian.davis@atlasschool.com>
2024-05-22 19:30:47 +0000

    test

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -13,6 +13,11 @@ void more_numbers(void)
 	for (lines = 1; lines < 10; lines++)
 	{
 		for (num = 0; num <= 14; num++)
+		{
+			if (num > 9)
+				_putchar(num / 10 + '0');
+				_putchar(num % 10 + '0');
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
