94bf2f362ccbbf9cf32bead51349435fbe9a2dfd
“Tristian <tristian.davis@atlasschool.com>
2024-05-22 20:24:20 +0000

    7test

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws a diagonal line on terminal
+ * @n: parameter
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		while (n > 0)
+		{
+			_putchar(" ");
+			n--;
+			if (n = 0)
+				_putchar('\\');
+				_putchar('\n');
+		}
+	}
+}
