26e33cc2068fca4e7587c85784c036d61b20d198
“Tristian <tristian.davis@atlasschool.com>
2024-05-23 01:00:36 +0000

    test

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *main - print numbers 1-100
+ *Return: 0
+ */
+
+int main(void)
+{
+	int num = 1;
+
+	while (num <= 100)
+	{
+		if (num % 3 == 0)
+		{
+			printf("Fizz\n");
+		}
+		else if (num % 5 == 0)
+		{
+			printf("Buzz\n");
+		}
+		else if ((num % 3 == 0) && (num % 5 == 0))
+		{
+			printf("FizzBuzz\n");
+		}
+		else
+		{
+			printf("%d\n, num");
+		}
+	}
+
+	return (0);
+
+}
