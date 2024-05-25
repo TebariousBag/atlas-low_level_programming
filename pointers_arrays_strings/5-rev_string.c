23da54af5a2f68bcb84ae86027ad878541920c0a
“Tristian <tristian.davis@atlasschool.com>
2024-05-25 18:41:21 +0000

    test

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -10,13 +10,15 @@ void rev_string(char *s)
 	int count;
 
 	for (index = 0; s[index] != '\0'; index++)
-	{
 		count++;
-	}
 
-	for (index = count -1; index >= 0; index--)
+	for (index = 0; index < count / 2; index++)
 	{
+		char temp;
+		temp = s[index];
+
+		s[index] = s[count - 1 - index];
+		s[count - 1 - index] = temp
 		_putchar(s[index]);
 	}
-	_putchar('\n');
 }
