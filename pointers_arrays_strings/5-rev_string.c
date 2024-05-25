5ae3090c741176a9130a3e05a7ea2c7c36c52ec7
“Tristian <tristian.davis@atlasschool.com>
2024-05-25 17:25:05 +0000

    5test

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: parameters
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int index = 0;
+
+	while (s[index] != '\0')
+	{
+		index++;
+	}
+	index --;
+	while (s[index] <= 0)
+	{
+		_putchar(s[index]);
+		index--;
+	}
+	_putchar('\n');
+}
