0458ae89459815d2d5ca90aa4b841bbdc94c123a
“Tristian <tristian.davis@atlasschool.com>
2024-05-26 18:44:20 +0000

    10

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _atoi - convert a string to integer
+ * @s: parameter
+ * Return: if no numbers in string then 0
+ */
+
+int _atoi(char *s)
+{
+	int sign = 1;
+	int result = 0;
+
+	while (*s == ' ')
+		s++;
+	if (*s == '-')
+	{
+		sign *= -1;
+		s++;
+	}
+	else if (*s == '+')
+	{
+		s++;
+	}
+	while (*s >= '0' && *s <= '9')
+	{
+		result = result * 10 + (*s - '0');
+		s++;
+	}
+	return (result * sign);
+}
