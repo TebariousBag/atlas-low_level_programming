47955fd6209d8a4ee683d809e7a69de9726b4902
“Tristian <tristian.davis@atlasschool.com>
2024-06-02 18:54:51 +0000

    6test

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words in string
+ * @taco: parameter
+ * Return: taco
+ */
+
+char *cap_string(char *taco)
+{
+	int count = 0;
+
+	while (taco[count != '\0']
+	{
+		while (!(taco[count] >= 'a' && taco[count] <= 'z')
+			count++;
+		if (taco[count - 1] == ' ' ||
+			taco[count - 1] == '\t' ||
+			taco[count - 1] == '\n' ||
+			taco[count - 1] == ',' ||
+			taco[count - 1] == ';' ||
+			taco[count - 1] == '.' ||
+			taco[count - 1] == '!' ||
+			taco[count - 1] == '?' ||
+			taco[count - 1] == '"' ||
+			taco[count - 1] == '(' ||
+			taco[count - 1] == ')' ||
+			taco[count - 1] == '{' ||
+			taco[count - 1] == '}' ||
+			count == 0)
+			
+			taco[count] -= 32;
+
+			count++;
+	}
+
+	return (taco);
+}
