7bee8c04549b36f66c2b7c5cb07b32a8f6144929
“Tristian <tristian.davis@atlasschool.com>
2024-06-02 17:51:50 +0000

    reverse

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses content of an array of integers
+ * @a: parameter array
+ * @n: parameter number of elements
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int count = 0;
+	int counter = n;
+	int temp;
+
+	while (count < counter)
+	{
+		temp = a[count];
+		a[count] = a[counter];
+		a[counter] = temp;
+		count++;
+		counter--;
+	}
+
+}
