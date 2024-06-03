9da4c3b856d083593b1ec94ac82908c6c545c574
“Tristian <tristian.davis@atlasschool.com>
2024-06-03 19:53:21 +0000

    5test

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ * @haystack: string
+ * @needle: substring
+ * Return: first occurence or null
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	char *h = haystack;
+	char *n = needle;
+
+	while (*haystack != '\0')
+	{
+		haystack++;
+
+		while (*h == *n && *n != '\0')
+		{
+			h++;
+			n++;
+		}
+		if (*n == '\0')
+			return (haystack);
+	}
+	return (NULL);
+}
