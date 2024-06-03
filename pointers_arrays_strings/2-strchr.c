acf82ddde5e278651e2f32c5c3d338b755ea7da2
“Tristian <tristian.davis@atlasschool.com>
2024-06-03 18:31:03 +0000

    test2

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: the string
+ * @c: the character
+ * Return: first occurence of character or null
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (s != '\0')
+	{
+		if (s == c)
+			return (s);
+		s++;
+	}
+	if (s == c)
+		return (s);
+	return (NULL);
+}
