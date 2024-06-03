a5b46ad2ee541fcdb0f7f49bbdf2e766980e7958
“Tristian <tristian.davis@atlasschool.com>
2024-06-03 19:30:14 +0000

    4test

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searches the string for any set of bytes
+ * @s: string 1
+ * @accept: string 2
+ * Return: s or null
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int *count = accept;
+
+	while (*s != '\0')
+	{
+		while (*count != '\0')
+		{
+			if (*s == *count)
+				return (s);
+				count++;
+		}
+		s++;
+	}
+	return (NULL);
+}
