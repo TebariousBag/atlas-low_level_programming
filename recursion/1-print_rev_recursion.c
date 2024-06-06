375716bca0210fb62c098dd7d31735e16c4dc7c5
“Tristian <tristian.davis@atlasschool.com>
2024-06-06 17:10:59 -0500

    think i understand

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -7,9 +7,10 @@
 
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s == '\0')	//base case
 	{
-		_print_rev_recursion(s + 1);
-		_putchar(*s);
+		return;	//to exit
 	}
+		_print_rev_recursion(s + 1);	//loop through until end
+		_putchar(*s);	//then start printing backwards
 }
