7364fcd09b33364bc794f6fd33a9998b6797b1cb
“Tristian <tristian.davis@atlasschool.com>
2024-06-07 14:09:50 -0500

    using helper function

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include "math.h"
+/**
+ * is_prime_number - checks if prime number
+ * @n: the number
+ * Return: 1 or 0
+ */
+
+int helper(int n, int i)	/* helper function*/
+{
+	if (i > sqrt(n))	/* if i is greater than square root of n than its prime*/
+	{
+		return (1);
+	}
+	if (n % i == 0)	/*if the remainder of n is 0 than it is not prime*/
+	{
+		return (0);
+	}
+	return helper(n, i + 1);	/* increases i by 1*/
+
+}
+int is_prime_number(int n)
+{
+	return helper(n, 2);	/* gives helper i the value of 2*/
+}
