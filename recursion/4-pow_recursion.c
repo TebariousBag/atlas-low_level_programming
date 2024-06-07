d788c1bdfed725755eb6fe4e1563de569e018fc0
“Tristian <tristian.davis@atlasschool.com>
2024-06-07 12:41:27 -0500

    again

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -8,17 +8,17 @@
 
 int _pow_recursion(int x, int y)
 {
-	if (y < 0)
+	if (y < 0)	/* base case no negative numbers*/
 	{
 		return (-1);
 	}
-	if (y == 0)
+	if (y == 0)	/* base case is 1*/
 	{
 		return (1);
 	}
 	else
 	{
-		return (x * _pow_recursion(y - 1));
+		return (x * _pow_recursion(x, y - 1)); /* x to the power of y*/
 	}
 
 }
