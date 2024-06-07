98b4520237d655e2ff241a50711843b8cc9bcf48
“Tristian <tristian.davis@atlasschool.com>
2024-06-07 11:42:07 -0500

    git

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -5,9 +5,8 @@
  * Return: 0 -1 or 1
  */
 
+int factorial(int n)
 {
-	int factorial(int n)
-
 	if (n < 0)	/* factorial can't be negative*/
 	{
 		return (-1);
@@ -18,6 +17,6 @@
 	}
 	else
 	{
-		return n * factorial(n - 1);	/* n times n - 1*/
+		return (n * factorial(n - 1));	/* n times n - 1*/
 	}
 }
