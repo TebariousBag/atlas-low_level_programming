0bb74aceedd7c3efb9d55987f5cd23c415e9c75e
“Tristian <tristian.davis@atlasschool.com>
2024-06-08 16:51:50 -0500

    liball

--- static_libraries/create_static_lib.sh
+++ static_libraries/create_static_lib.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
 gcc -Wall -pedantic -Werror -Wextra -c *.c
-ar -rc libmy.a * .o
-ranlib libmy.a
-ar -t libmy.a
-nm libmy.a
+ar -rc liball.a * .o
+ranlib liball.a
+ar -t liball.a
+nm liball.a
