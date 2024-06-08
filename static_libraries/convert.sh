3e795b13e06f8b43bb933cfa2b344b0b8e2c763b
“Tristian <tristian.davis@atlasschool.com>
2024-06-08 16:05:11 -0500

    convert

--- static_libraries/convert.sh
+++ static_libraries/convert.sh
@@ -1,4 +1,8 @@
 #/bin/bash
+
+cd /Users/tebariousbag/school/atlas-low_level_programming/atlas-low_level_programming-1/static_libraries
+
 for file in *.c; do
     gcc -c "$file"
 done
+echo "All .c files compiled to .o files"
