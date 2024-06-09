33780a553f1fcd1af2c62c103cec95daa29edd26
“Tristian <tristian.davis@atlasschool.com>
2024-06-08 20:10:17 -0500

    2

---
+++ argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * 
+ * 
+ */
+
+int main(int argc, *argv[])
+{
+	int count;
+
+	for (count = 0; count > argc; count--;)
+	{
+		printf("%s\n", argv[count]);
+	}
+	return (0);
+}
