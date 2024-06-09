da538c6470025bdb1df3d1d8a36f033ae33e49b7
“Tristian <tristian.davis@atlasschool.com>
2024-06-08 20:59:10 -0500

    3

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - multiplies 2 numbers
+ * @argc: parameter 1
+ * @argv: parameter 2
+ * Return: 0
+ */
+
+int main(int argc; char* argv[])
+{
+	int multiplied = argv[1] * argv[2];
+	if (argc != 3)
+	{
+		printf("Error\n")
+		return (1);
+	}
+	printf("%d\n", multiplied);
+	return (0);
+
+}
