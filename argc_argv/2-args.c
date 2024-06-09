53130064beee15bd4a57ed491e4b47e409c3a11a
“Tristian <tristian.davis@atlasschool.com>
2024-06-08 20:17:09 -0500

    maybe

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -8,9 +8,9 @@ int main(int argc, char *argv[])
 {
 	int count;
 
-	for (count = 1; count <= argc; count++)
+	for (count = 1; count <= argc; count++)	/*start counting at 1*/
 	{
-		printf("%s\n", argv[count]);
+		printf("%s\n", argv[count - 1]);	/*print argv count -1 because it starts at 0*/
 	}
 	return (0);
 }
