e7d164d232bb08a9c0911a875ef120ff51bf956b
“Tristian <tristian.davis@atlasschool.com>
2024-06-08 19:40:21 -0500

    0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - function prints name
+ * @argc: arg count
+ * @argv: arg vector
+ * Return: name
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc > 0)	/*if its more than 0 there is at least 1 argument*/
+	{
+		printf("%s\n", argv[0]);
+	}
+}
