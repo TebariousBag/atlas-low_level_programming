74a84e23cac59b2686cfd3b52af15dc16bbb962b
“Tristian <tristian.davis@atlasschool.com>
2024-06-08 21:23:12 -0500

    can use other libraries

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * main - multiplies 2 numbers
  * @argc: parameter 1
@@ -8,13 +10,11 @@
 
 int main(int argc; char *argv[])
 {
-	int multiplied = argv[1] * argv[2];
-	if (argc != 3)
+	if (argc != 3)	/*need 2 arguments else error*/
 	{
 		printf("Error\n")
 		return (1);
 	}
-	printf("%d\n", multiplied);
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
-
 }
