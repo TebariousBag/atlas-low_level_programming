70433d85ce4a4d6c8f5fcc7924144b48e8bfc0fe
“Tristian <tristian.davis@atlasschool.com>
2024-06-08 19:55:40 -0500

    print num

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
 {
 	(void)argv;	/*can I just void this?*/
 	int num = argc -1;	/* argc prints its name first so we have to subtract 1*/
-	printf("%d\n", argc);
+	printf("%d\n", num);
 
 	return (0);
 }
