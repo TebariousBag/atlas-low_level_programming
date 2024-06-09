90d12b4fa850d0666effbd82eafd56edd7f75789
“Tristian <tristian.davis@atlasschool.com>
2024-06-08 21:27:18 -0500

    now i need semicolon

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
 {
 	if (argc != 3)	/*need 2 arguments else error*/
 	{
-		printf("Error\n")
+		printf("Error\n");
 		return (1);
 	}
 	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
