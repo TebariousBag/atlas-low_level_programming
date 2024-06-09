fb79a4ac7fb425b13003517fd5689de6e9e409c3
“Tristian <tristian.davis@atlasschool.com>
2024-06-08 20:00:59 -0500

    1

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,13 +1,16 @@
 #include "main.h"
 /**
- * 
- * 
- */
+*main - counts arguments
+*@argc: parameter
+*@argv: parameter
+Return: arg count num
+*/
 
 int main(int argc, char *argv[])
 {
-	(void)argv;	/*can I just void this?*/
 	int num = argc -1;	/* argc prints its name first so we have to subtract 1*/
+
+	(void)argv;	/*can I just void this?*/
 	printf("%d\n", num);
 
 	return (0);
