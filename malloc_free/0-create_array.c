5d6dc078342f6967871f0dd745766d8d3fa90224
“Tristian <tristian.davis@atlasschool.com>
2024-06-13 13:22:06 -0500

    0-create

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - creates an array of chars and initializes it with a specific char
+ * @size: size of the array
+ * @c: the char
+ * Return: pointer to array or Null
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	
+}
