2151b8e7db6dd91af4b5bd5cd2d05cd0204badcf
“Tristian <tristian.davis@atlasschool.com>
2024-06-14 16:59:52 -0500

    freegrid

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * free_grid - frees grid created previously
+ * @grid: the grid
+ * @height: the height
+ * Return: void
+ */
+void free_grid(int **grid, int height)
+{
+	int row;
+
+	for (row = 0; row < heighyt; row++)
+	{
+		free(grid[row]);
+	}
+	free(grid);
+}
