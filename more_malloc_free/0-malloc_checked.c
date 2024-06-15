b6585cfe3d2522deaff749dd786f3c916a95624a
“Tristian <tristian.davis@atlasschool.com>
2024-06-15 13:34:25 -0500

    malloc checked

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,1 +1,21 @@
 #include "main.h"
+#include <stdio.h>
+/**
+ * malloc_checked - allocates memory
+ * @b: parameter of bytes
+ * Return: pointer to memory or 98
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;	/*pointer to any data type*/
+
+	ptr = malloc(b);	/*make memory for b*/
+
+	if (ptr == NULL)	/*if failed exit 98*/
+	{
+		exit(98);
+	}
+
+	return (ptr);	/*return pointer to memory*/
+}
