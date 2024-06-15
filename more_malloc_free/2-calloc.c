8242bc676a421d2e6258bfedc6c657c5640554df
“Tristian <tristian.davis@atlasschool.com>
2024-06-15 15:32:10 -0500

    calloc

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * calloc - allocate memory for an array
+ * @nmeb: parameter number of elements
+ * @size: parameter of bytes
+ * Return: pointer to the memory or null
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+	char *zero;
+	unsigned int count;
+
+	if (nmemb == 0 || size == 0)	/*if elements and bytes are zero than null*/
+	{
+		return (NULL);
+	}
+
+	ptr = malloc(nmemb * size);	/*memory for elements and bytes*/
+
+	if (ptr == NULL)	/*if failed than null*/
+	{
+		return (NULL);
+	}
+
+	zero = ptr;	/*pointer to ptr that replaces with zero*/
+
+	for (count = 0; count < nmemb * size; count++)	/*cycle through*/
+	{
+		*(zero++) = 0;	/*as it cycles zero replaces it with 0's*/
+	}
+
+	return (ptr);	/*return the pointer all 0's*/
+
+}
