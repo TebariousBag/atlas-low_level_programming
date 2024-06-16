3a76f7d1cb6afe73a7ea5c7387941e3e11d44215
“Tristian <tristian.davis@atlasschool.com>
2024-06-16 13:03:06 -0500

    base file

---
+++ preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print name of file it was compiled from, new line
+ * Return: void
+ */
+
+int main(void)
+{
+	printf("%s\n" __BASE_FILE__);
+	
+	return (0);
+}
