c18513c6a8ecd68005158e69f28c4d5e129352da
“Tristian <tristian.davis@atlasschool.com>
2024-06-23 10:36:00 -0500

    testing

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dogs info
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: the dogs owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
