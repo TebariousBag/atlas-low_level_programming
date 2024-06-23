2e42435a21120567fb8ebe07ef4e81dec84759f1
“Tristian <tristian.davis@atlasschool.com>
2024-06-23 11:43:28 -0500

    changes

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -11,21 +11,32 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 	{
-		return (NULL);
+		return;
 	}
 	if (d->name == NULL)
 	{
 		printf("Name: (nil)\n");
-		else
-			printf("Name: %s\n", d->name);
-		if (d->age < 0)
-			printf("Age: (nil)\n");
-		else
-			printf("Age: %f\n", d->age);
-		if (d->owner == NULL)
-			printf("Owner: (nil)\n");
-		else
-			printf("Owner: %s\n", d->owner);
+	}
+	else
+	{
+		printf("Name: %s\n", d->name);
+	}
+	if (d->age < 0)
+	{
+		printf("Age: (nil)\n");
+	}
+	else
+	{
+		printf("Age: %f\n", d->age);
+	}
+	if (d->owner == NULL)
+	{
+		printf("Owner: (nil)\n");
+	}
+	else
+	{
+		printf("Owner: %s\n", d->owner);
+	}
 
 	}
 
