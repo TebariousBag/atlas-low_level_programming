3b1f2742f3d0bc0e49fb13930561d4066359d431
“Tristian <tristian.davis@atlasschool.com>
2024-06-23 15:21:11 -0500

    execute array

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes a function on each element of array
+ * @array: the array
+ * @size: the size
+ * @action: the function to be executed
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int cycle;
+
+	if (!array || !action)	/*check to see if fails then return*/
+	{
+		return;
+	}
+	for (cycle = 0; cycle < size; cycle++)
+	{
+		action(array[cycle]);
+	}
+}
