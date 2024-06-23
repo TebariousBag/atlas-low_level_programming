3d8e8ca42e3425d0d90fda6b1989943f02cbd028
“Tristian <tristian.davis@atlasschool.com>
2024-06-23 15:38:15 -0500

    test

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an int
+ * @array: the array we are searching
+ * @size: size of array
+ * @cmp: pointer to compare values
+ * Return: index of first element or -1
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	unsigned int cycle;
+
+	if (!array || !cmp)
+	{
+		return (-1);
+	}
+	for (cycle = 0; cycle < size; cycle++)
+	{
+		if (cmp(array[cycle] != 0))
+		{
+			return (cycle);
+		}
+	}
+}
