fd8a86c6f6eef7d1a72b1a006745b9abe320952a
“Tristian <tristian.davis@atlasschool.com>
2024-06-24 13:47:32 -0500

    worked just had to shorten line for betty

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -20,7 +20,8 @@ int sum_them_all(const unsigned int n, ...)
 	}
 	for (t = 0; t < n; t++)	/*iterating through each arg*/
 	{
-		sum += va_arg(mylist, int);	/*receiving each arg and adding and saving to sum*/
+		sum += va_arg(mylist, int);	
+		/*receiving each arg and adding and saving to sum*/
 	}
 	va_end(mylist);	/*frees up mylist*/
 	return (sum);	/*returns all the numbers added*/
