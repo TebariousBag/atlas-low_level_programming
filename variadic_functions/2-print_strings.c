1e8ad697e8bea53d62f584f087a12243e82b6142
“Tristian <tristian.davis@atlasschool.com>
2024-06-24 15:30:13 -0500

    print strings

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+/**
+ * print_strings - prints strings followed by newline
+ * @separator: what to be printed between strings
+ * @n: number of strings to be printed
+ * Return: void
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int t;
+	char *string;	/*what will store our string*/
+
+	va_list mylist;	/*list to hold*/
+
+	va_start(mylist, n);	/*start list*/
+
+	for (t = 0; t < n; t++)	/*iterate through*/
+	{
+		string = va_arg(mylist, char *);
+		if (!string)	/*if string is null then it equals nil*/
+			string = "nil";
+		if (separator != NULL && t > 0)	
+		/*if separator is not null and its greater than zero print separator*/
+			printf("%s", separator);
+		else	/*else print string*/
+			printf("%s", string);
+	}
+	va_end(mylist); /*free mylist*/
+	printf("\n");
+}
