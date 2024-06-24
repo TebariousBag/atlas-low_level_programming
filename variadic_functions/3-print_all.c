6c8275cc85f4e444c787e8f218e9a3c56a34bbc1
“Tristian <tristian.davis@atlasschool.com>
2024-06-24 16:23:28 -0500

    changes

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,11 @@
+#include "variadic_functions.h"
+/**
+ * print_all - prints all types
+ * @format: list of types of arguments given to funct
+ */
+
+void print_all(const char * const format, ...)
+{
+	int t;
+	char s
+}
