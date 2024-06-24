2791c8f0cb9f33c0a704b88eee0f785b97003540
“Tristian <tristian.davis@atlasschool.com>
2024-06-24 17:24:30 -0500

    strelen

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -58,13 +58,13 @@ dog_t *new_dog(char *name, float age, char *owner)
 	{
 		return (NULL);
 	}
-	mydog->name = malloc(sizeof(char) * (_strlen(name) +1));
+	mydog->name = malloc(sizeof(char) * (_strlen(name) +1 ));
 	if (mydog->name == NULL)
 	{
 		free(mydog);
 		return (NULL);
 	}
-	mydog->owner = malloc(sizeof(char) * (_strelen(owner) +1));
+	mydog->owner = malloc(sizeof(char) * (_strlen(owner) +1 ));
 	if (mydog->owner == NULL)
 	{
 		free(mydog->name);
