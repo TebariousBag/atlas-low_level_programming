c9157143a0b368cb3a12df7a441e3bac3f211123
“Tristian <tristian.davis@atlasschool.com>
2024-06-24 18:47:19 -0500

    parenthesis

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -2,30 +2,30 @@
 #include <stdlib.h>
 
 /**
- * get_op_func - function
- * @s: parameter
- * Return: pointer to function
- */
+* get_op_func - function
+* @s: parameter
+* Return: pointer to function
+*/
 
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
 
-    int i = 0;
+	int i = 0;
 
-    while (ops[i].op != NULL)
-    {
-        if (strcmp(s, ops[i].op == 0))
-            return (ops[i].f);
-        i++;
-    }
+	while (ops[i].op != NULL)
+	{
+		if (strcmp(s, ops[i].op) == 0))
+			return (ops[i].f);
+		i++;
+	}
 
-    return (0);
+	return (0);
 }
