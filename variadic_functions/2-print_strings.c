85fd6f25b30bcbc182f0f31d5619e1b259b42941
“Tristian <tristian.davis@atlasschool.com>
2024-06-24 15:37:29 -0500

    tabbed too far

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -1,14 +1,15 @@
 #include "variadic_functions.h"
 /**
- * print_strings - prints strings followed by newline
- * @separator: what to be printed between strings
- * @n: number of strings to be printed
- * Return: void
- */
+* print_strings - prints strings followed by newline
+* @separator: what to be printed between strings
+* @n: number of strings to be printed
+* Return: void
+*/
 
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int t;
+
 	char *string;	/*what will store our string*/
 
 	va_list mylist;	/*list to hold*/
@@ -20,7 +21,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		string = va_arg(mylist, char *);
 		if (!string)	/*if string is null then it equals nil*/
 			string = "nil";
-		if (separator != NULL && t > 0)	
+		if (separator != NULL && t > 0)
 		/*if separator is not null and its greater than zero print separator*/
 			printf("%s", separator);
 		else	/*else print string*/
