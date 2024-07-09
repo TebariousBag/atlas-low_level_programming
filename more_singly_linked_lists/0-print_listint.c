5c6e3e31688e9907c1a12966af051e06a624a5da
“Tristian <tristian.davis@atlasschool.com>
2024-07-08 21:15:05 -0500

    mainfiles

---
+++ more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints the elements of a list
+ * @h: head of a list
+ *
+ * Return: number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h != NULL)
+	{
+		nodes++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (nodes);
+}
