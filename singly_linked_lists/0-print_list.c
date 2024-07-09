cbf817fb03a3de0fe491eddadb5e31f459dfd653
“Tristian <tristian.davis@atlasschool.com>
2024-07-08 19:22:29 -0500

    print list

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -2,10 +2,25 @@
 /**
  * print_list - prints all elements of list_t
  * @h: the list
- * Return:
+ * Return: the number of nodes
  */
 
 size_t print_list(const list_t *h)
 {
+	size_t nodes;
 
+	while (h)
+	{
+		if (h->str == NULL);
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+			nodes++;
+			h = h->next;
+		}
+	}
+	return (nodes);
 }
