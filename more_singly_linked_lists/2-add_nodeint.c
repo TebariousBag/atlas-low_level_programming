b7dd36bea3fb3deba2ac5baca314a488d59c2fb2
“Tristian <tristian.davis@atlasschool.com>
2024-07-08 21:28:43 -0500

    add new node

---
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * add_nodeint - add a new node at the beginning of a linked list
+ * @head: pointer to head of list
+ * @n: n element
+ *
+ * Return: NULL if it fails
+ *         Otherwise - the address of the new element
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = *head;
+	*head = new;
+
+	return (*head);
+}
