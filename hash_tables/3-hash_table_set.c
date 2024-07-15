45e884d8a2811f390630b6edfcba43061a1880ba
“Tristian <tristian.davis@atlasschool.com>
2024-07-15 18:29:03 -0500

    insert node

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - add an element to the hash table
+ * @ht: the table to add
+ * @key: is the key
+ * @value: is the value associated with the key
+ * Return: 1 is success or 0
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	
+}
