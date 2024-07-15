824f34bb65c2a31bcdf3a61f3554581ce449eade
“Tristian <tristian.davis@atlasschool.com>
2024-07-15 18:53:11 -0500

    delete

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+/**
+ * delete_dnodeint_at_index - deletes node at index of list
+ * @head: head of list
+ * @index: the node to delete
+ * Return: 1 or -1
+ */
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *tmp = *head;
+
+	if (*head == NULL)
+	return (-1);
+	while (index > 0)
+	{
+		if (tmp == NULL)
+			return (-1);
+		tmp = tmp->next;
+		index--;
+	}
+	if (tmp == *head)
+	{
+		*head = tmp->next;
+		(*head)->prev = NULL;
+	}
+	else
+	{
+		tmp->prev->next = tmp->prev;
+		if (tmp->next)
+			tmp->next->prev = tmp->prev;
+	}
+	free(tmp);
+	return (1);
+}
