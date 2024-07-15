df9324551d08bc3200369a8c3bb83ab76404325f
“Tristian <tristian.davis@atlasschool.com>
2024-07-15 15:58:17 -0500

    hash tables

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - create a hash table
+ * @size: the size of the array
+ * Return: pointer to the hash table or NULL
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;	/*create hash table*/
+	unsigned long int i;	/*iterator*/
+
+	ht = malloc(sizeof(hash_table_t));	/*allocate memory for hash table*/
+
+	if (ht == NULL)	/*case of null*/
+	{
+		return (NULL);
+	}
+
+	ht->size = size;	/*size equals variable size*/
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	/*make space for the array*/
+
+	if (ht->array == NULL);	/*if array empty*/
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		ht->array[i] = NULL;	/*add null to each spot to create the table*/
+	}
+
+	return (ht);
+}
