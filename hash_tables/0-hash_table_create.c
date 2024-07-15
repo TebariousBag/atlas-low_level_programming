a3067408ab98f1d31a028b4090b8e4eb5f9b8758
“Tristian <tristian.davis@atlasschool.com>
2024-07-15 15:59:23 -0500

    removed semicolon from statement

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -21,7 +21,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	ht->array = malloc(sizeof(hash_node_t *) * size);
 	/*make space for the array*/
 
-	if (ht->array == NULL);	/*if array empty*/
+	if (ht->array == NULL)	/*if array empty*/
 	{
 		return (NULL);
 	}
